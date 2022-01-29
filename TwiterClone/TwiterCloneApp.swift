d54dc77b3371cd12053a6d1896476f57bfa4c523
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-01-29 22:09:47 +0530

    Initial Commit

---
+++ TwiterClone/TwiterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiterCloneApp.swift
+//  TwiterClone
+//
+//  Created by shehan karunarathna on 2022-01-29.
+//
+
+import SwiftUI
+
+@main
+struct TwiterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
